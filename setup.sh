e0f25a0e35c5983814aa714dbe645293c656f3ac
Gouthamm <80330390+TheBoyHasNoName@users.noreply.github.com>
2021-06-19 21:22:19 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[general]  
-email = \"me@gmail.com\""  > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = true"  >> ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
